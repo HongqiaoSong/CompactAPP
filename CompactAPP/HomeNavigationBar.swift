f5d1f4cabfc742d8bea1ec8d8b503acc70666885
Songhongqiao <1910370151j@kindai.ac.jp>
2021-07-18 15:55:03 +0900

    HomeNavigation増加、細かいところを修正する

---
+++ CompactAPP/HomeNavigationBar.swift
@@ -0,0 +1,20 @@
+//
+//  HomeNavigationBar.swift
+//  CompactAPP
+//
+//  Created by songhongqiao on 2021/07/18.
+//
+
+import SwiftUI
+
+struct HomeNavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeNavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeNavigationBar()
+    }
+}
---
+++ CompactAPP/PostDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PostDetailView.swift
+//  CompactAPP
+//
+//  Created by songhongqiao on 2021/07/18.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostDetailView()
+    }
+}
